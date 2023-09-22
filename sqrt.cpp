016faf2a0770d7d6b1360f3ddbbc34dfdbda504f
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-09-22 21:32:58 +0300

    Update sqrt.cpp

--- sqrt.cpp
+++ sqrt.cpp
@@ -1,10 +1,14 @@
 int itc_sqrt(int num)
 {
 	int a = -1;
-	for(int i = 1; i <num ; i++)
+	for (int i = 1; i < num; i++)
 	{
 		if (i * i == num)
 			a = i;
 	}
+	if (num == 1)
+		a = 1;
+	else if (num == 0)
+		a = 0;
 	return a;
 }
