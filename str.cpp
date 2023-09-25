b118906745dba7267ed22b62103345144e7a7fbd
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-09-25 18:53:36 +0300

    Update str.cpp

--- str.cpp
+++ str.cpp
@@ -1,4 +1,17 @@
-#include 'easy.h'
+int itc_sqrt(int num)
+{
+	int a = -1;
+	for (int i = 1; i < num; i++)
+	{
+		if (i * i == num)
+			a = i;
+	}
+	if (num == 1)
+		a = 1;
+	else if (num == 0)
+		a = 0;
+	return a;
+}
 int itc_str(int a, int b, int c)
 {
 	if ((a + b > c && a + c > b && b + c > a) && (a && b && c > 0))
